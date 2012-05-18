73816854e50e378696bf528259ea6bd96e58de4d
lemonhall <lemonhall@gmail.com>
2012-05-18 21:14:53 +0800

    first commit

---
+++ contentscript.js
@@ -0,0 +1,9 @@
+﻿/*
+ * Copyright (c) 2010 The Chromium Authors. All rights reserved.  Use of this
+ * source code is governed by a BSD-style license that can be found in the
+ * LICENSE file.
+ */
+(function(){
+	var subjs=$("a[href*='http://www.douban.com/subject/']");
+	subjs.parent().parent().parent().hide(); 
+} )();
